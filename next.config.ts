5ba84bcbe91c431ba8a7b5e9ecf20a44cca9b24a
Goutham Raj <graj110895@gmail.com>
2025-09-27 19:14:10 +0530

    Fix build config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"]
+    }
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }
+}
+
+module.exports = nextConfig
